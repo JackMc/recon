2a533edaca79cafb00e6b9814c4a269c9dfc9551
Jack McCracken <jackmc@hey.com>
2022-03-26 09:25:20 -0400

    Update Rails to 6.1.5

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,101 +11,100 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_02_07_235726) do
+
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string("name", null: false)
-    t.string("record_type", null: false)
-    t.bigint("record_id", null: false)
-    t.bigint("blob_id", null: false)
-    t.datetime("created_at", null: false)
-    t.index(["blob_id"], name: "index_active_storage_attachments_on_blob_id")
-    t.index(["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness",
-unique: true)
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string("key", null: false)
-    t.string("filename", null: false)
-    t.string("content_type")
-    t.text("metadata")
-    t.string("service_name", null: false)
-    t.bigint("byte_size", null: false)
-    t.string("checksum", null: false)
-    t.datetime("created_at", null: false)
-    t.index(["key"], name: "index_active_storage_blobs_on_key", unique: true)
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci",
-force: :cascade do |t|
-    t.bigint("blob_id", null: false)
-    t.string("variation_digest", null: false)
-    t.index(["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true)
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "domains", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text("fqdn")
-    t.string("source")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
-    t.bigint("target_id")
-    t.bigint("source_scan_id")
-    t.boolean("wildcard")
-    t.index(["source_scan_id"], name: "index_domains_on_source_scan_id")
-    t.index(["target_id", "fqdn"], name: "index_domains_on_target_id_and_fqdn", unique: true, length: { fqdn: 256 })
-    t.index(["target_id"], name: "index_domains_on_target_id")
+    t.text "fqdn"
+    t.string "source"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "target_id"
+    t.bigint "source_scan_id"
+    t.boolean "wildcard"
+    t.index ["source_scan_id"], name: "index_domains_on_source_scan_id"
+    t.index ["target_id", "fqdn"], name: "index_domains_on_target_id_and_fqdn", unique: true, length: { fqdn: 256 }
+    t.index ["target_id"], name: "index_domains_on_target_id"
   end
 
   create_table "http_probes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint("domain_id")
-    t.text("http_response", size: :long)
-    t.string("url")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
-    t.integer("status_code")
-    t.string("status_name")
-    t.boolean("https")
-    t.text("body", size: :long)
-    t.json("headers")
-    t.text("raw_body", size: :long)
-    t.bigint("scan_id")
-    t.string("failure_reason")
-    t.boolean("failed")
-    t.text("http_request", size: :long)
-    t.index(["domain_id"], name: "index_http_probes_on_domain_id")
+    t.bigint "domain_id"
+    t.text "http_response", size: :long
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "status_code"
+    t.string "status_name"
+    t.boolean "https"
+    t.text "body", size: :long
+    t.json "headers"
+    t.text "raw_body", size: :long
+    t.bigint "scan_id"
+    t.string "failure_reason"
+    t.boolean "failed"
+    t.text "http_request", size: :long
+    t.index ["domain_id"], name: "index_http_probes_on_domain_id"
   end
 
   create_table "scans", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string("seed_type")
-    t.bigint("seed_id")
-    t.string("description")
-    t.json("metadata")
-    t.string("status")
-    t.datetime("created_at", precision: 6)
-    t.datetime("updated_at", precision: 6)
-    t.bigint("target_id")
-    t.string("type")
-    t.index(["seed_type", "seed_id"], name: "index_scans_on_scan_source")
+    t.string "seed_type"
+    t.bigint "seed_id"
+    t.string "description"
+    t.json "metadata"
+    t.string "status"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.bigint "target_id"
+    t.string "type"
+    t.index ["seed_type", "seed_id"], name: "index_scans_on_scan_source"
   end
 
   create_table "tagged_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string("item_type")
-    t.bigint("domain_id")
-    t.bigint("tag_id")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
-    t.index(["domain_id", "tag_id"], name: "index_tagged_items_on_domain_id_and_tag_id", unique: true)
+    t.string "item_type"
+    t.bigint "domain_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["domain_id", "tag_id"], name: "index_tagged_items_on_domain_id_and_tag_id", unique: true
   end
 
   create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string("name")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "targets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text("name")
-    t.text("description")
-    t.datetime("created_at", precision: 6, null: false)
-    t.datetime("updated_at", precision: 6, null: false)
+    t.text "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
